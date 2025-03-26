90c389096bbea7476af2364ddb8a760f5f592207
lingqiz <zlqzcc@gmail.com>
2025-03-26 16:26:20 -0400

    rename

--- lnopt_run.sh
+++ lnopt_run.sh
@@ -22,6 +22,6 @@ NEP=16
 LNR=0.0005
 AVG=true
 
-python3 linear.py --recon_method $MTD --loss_type $LST --decay_rate $DCR \
-                  --n_sample $NSP --data_path $DST --model_path $MDP \
-                  --lr $LNR --batch_size $BSZ --n_epoch $NEP --avg $AVG
+python3 train_olm.py --recon_method $MTD --loss_type $LST --decay_rate $DCR \
+                     --n_sample $NSP --data_path $DST --model_path $MDP \
+                     --lr $LNR --batch_size $BSZ --n_epoch $NEP --avg $AVG
